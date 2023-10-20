0089713c20e0ad1fa894c41e2f04c15f8e6fe690
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-10-20 16:00:02 +0200

    Configure spf13/cobra entrypoint (#3)

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
 
 func main() {
-	fmt.Println("pivo db")
+	cmdRoot :=
+		&cobra.Command{
+			Use:     "pivo",
+			Short:   "A sample of pivo usage.",
+			Example: "",
+			Version: "0.0.1-SNAPSHOT",
+		}
+
+	err := cmdRoot.Execute()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+		return
+	}
 }
